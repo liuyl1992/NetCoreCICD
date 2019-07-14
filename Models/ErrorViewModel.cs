171103a21aa271ba0788cf5976fe5101c55c94a2
chaunce <757365800@qq.com>
2019-07-14 11:19:37 +0800

    dffff

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NetCoreCICD.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
